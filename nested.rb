5306e75a03d0bd6c25d8e8441ff06b42343cea13
Michael Waldo <mbwcogic@yahoo.com>
2019-09-29 21:03:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ programmer_hash=
           :languages => ["C"]
         }
      }
-    programmer_hash[yukihiro_matsumoto] = {
+    programmer_hash[:yukihiro_matsumoto] = {
    :known_for => "Ruby",
    :languages => ["LISP", "C"]
       
