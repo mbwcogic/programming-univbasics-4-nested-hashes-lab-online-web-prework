1eabd098e66c2bfd377605b498bf34ce25a0bca8
Michael Waldo <mbwcogic@yahoo.com>
2019-09-29 19:16:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,11 @@ def hopper
         }
      }
 
-hopper = {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]}
+hopper = {
+           :known_for => "COBOL", 
+           :languages => ["COBOL", "FORTRAN"]
+  
+         }
 end
 
 def alan_kay_is_known_for
