2e9a5c397f6df5576e5ebb6f856304c3541dc938
Michael Waldo <mbwcogic@yahoo.com>
2019-09-29 20:11:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,7 +70,6 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -85,7 +84,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] =  :known_for => "Ruby", :languages => ["LISP", "C"]
+adding_matz:yukihiro_matsumoto.include =  :known_for => "Ruby", :languages => ["LISP", "C"]
     
 end
 
