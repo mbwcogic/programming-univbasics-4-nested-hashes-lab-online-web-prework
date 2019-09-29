8535b40cc0dcd36308a23a7718cf994216b23a5e
Michael Waldo <mbwcogic@yahoo.com>
2019-09-29 22:05:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
    changing_alan[:alan_kay][:known_for] = "GUI"
-  alan_kay
+  changing_alan
 end
 
 def adding_to_dennis
