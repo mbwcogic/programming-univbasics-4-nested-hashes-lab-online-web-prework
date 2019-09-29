295f1d7d2b0b5a11b9e8b244aac166cf44c4cce0
Michael Waldo <mbwcogic@yahoo.com>
2019-09-29 19:36:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,7 +41,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     alan_kay_is_known_for= :alan_kay => {:known_for => "Object Orientation"},
+     alan_kay_is_known_for= "Object Orientation"},
 end
 
 def dennis_ritchies_language
