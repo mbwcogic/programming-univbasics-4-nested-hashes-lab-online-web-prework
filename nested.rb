b67ce4837389d14ce76dea707461b240f961ddc8
Michael Waldo <mbwcogic@yahoo.com>
2019-09-29 22:37:15 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,11 @@ def hopper
         }
      }
 
-
+hopper = {
+           :known_for => "COBOL", 
+           :languages => ["COBOL", "FORTRAN"]
+  
+         }
 end
 
 def alan_kay_is_known_for
@@ -37,6 +41,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     alan_kay_is_known_for= "Object Orientation"
 end
 
 def dennis_ritchies_language
@@ -55,6 +60,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     dennis_ritchies_language = "C"
 end
 
 def adding_matz
@@ -63,8 +69,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
-	programmer_hash = 
+programmer_hash=
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -79,8 +84,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+    programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+      
+    }
+    programmer_hash
 end
 
 def changing_alan
@@ -101,8 +110,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+   programmer_hash[:alan_kay][:known_for] = alans_new_info 
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] = [1, "Assembly"]
+programmer_hash
 end
